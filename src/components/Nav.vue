22017abacc3c91bba92e00d1415a5e242f28aef7
潘诚宽 <chengkuan0327@163.com>
2021-08-06 23:03:18 +0800

    实现 Nav 的激活状态

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"></Icon>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"></Icon>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"></Icon>
       统计
     </router-link>
@@ -27,7 +27,6 @@
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
     flex-direction: row;
     font-size: 12px;
-
     > .item {
       width: 33.33333%;
       display: flex;
@@ -35,10 +34,12 @@
       justify-content: center;
       align-items: center;
       padding: 2px 0;
-
       .icon {
         width: 32px;
         height: 32px;
+      }
+      &.selected {
+        color: crimson;
       }
     }
   }
