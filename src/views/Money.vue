2db6c720fac82fca16605bafa680ad24a0f9e2b0
潘诚宽 <chengkuan0327@163.com>
2021-08-10 16:16:25 +0800

    可以选中和取消 tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,18 +3,23 @@
     <NumberPad></NumberPad>
     <Types></Types>
     <Notes></Notes>
-    <Tags></Tags>
+    <Tags :data-source="tags"></Tags>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 export default {
   name: 'Money',
-  components: {Tags, Notes, Types, NumberPad}
+  components: {Tags, Notes, Types, NumberPad},
+  data() {
+    return {
+      tags: ['衣', '食', '住', '行', '玩']
+    }
+  }
 };
 </script>
 
