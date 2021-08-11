d702094fb6ae9096436fab04c2b170aa88a70250
潘诚宽 <chengkuan0327@163.com>
2021-08-11 15:51:24 +0800

    展示和创建标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,8 +14,11 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
-  import model from '@/model';
+  import recordListModel from '@/models/recordListModel';
+  import tagListModel from '@/models/tagListModel';
 
+  const recordList = recordListModel.fetch();
+  const tagList = tagListModel.fetch();
 
   window.localStorage.setItem('version', '0.0.1');
 
@@ -23,11 +26,11 @@
     components: {Tags, Notes, Types, NumberPad}
   })
   export default class Money extends Vue{
-    tags = ['衣', '食', '住', '行', '玩'];
+    tags = tagList;
+    recordList: RecordItem[] = recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
-    recordList = model.fetch();
 
     onUpdateTags(value: string[]) {
       this.record.tags = value;
@@ -36,13 +39,13 @@
       this.record.notes = value;
     }
     saveRecord() {
-      const record2: RecordItem = model.clone(this.record);
+      const record2: RecordItem = recordListModel.clone(this.record);
       record2.createdAt = new Date();
       this.recordList.push(record2);
     }
     @Watch('recordList')
     onRecordListChange() {
-      model.save(this.recordList);
+      recordListModel.save(this.recordList);
     }
   }
 </script>
