9aed9af97bcca70b945ec0d2a3b6cfd267a06c73
潘诚宽 <chengkuan0327@163.com>
2021-08-12 13:19:20 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -41,13 +41,11 @@
       this.record.notes = value;
     }
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+      recordListModel.create(this.record);
     }
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
