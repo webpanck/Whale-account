2ba64c6315ce9f6520ab4de0d7d0ebbd87077b30
潘诚宽 <chengkuan0327@163.com>
2021-08-16 15:23:13 +0800

    完成列表的 CSS

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -43,7 +43,7 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,18 @@
     <Layout>
       <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"></Tabs>
       <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"></Tabs>
+      <ol>
+        <li v-for="(group, index) in result" :key="index">
+          <h3 class="title">{{group.title}}</h3>
+          <ol>
+            <li class="record" v-for="item in group.items" :key="item.id">
+              <span>{{tagString(item.tags)}}</span>
+              <span class="notes" :style="{marginRight:'auto'}">{{item.notes}}</span>
+              <span>¥{{item.amount}}</span>
+            </li>
+          </ol>
+        </li>
+      </ol>
     </Layout>
 </template>
 
@@ -16,6 +28,28 @@
     components: {Tabs}
   })
   export default class Statistics extends Vue {
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.join(',');
+    }
+    get recordList() {
+      return this.$store.state.recordList;
+    }
+    get result() {
+      const {recordList} = this;
+      type HashTableValue = {title: string, items: RecordItem[]};
+      const hashTable: {[key: string]: HashTableValue} = {};
+      for(let i=0; i<recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split('T');
+        hashTable[date] = hashTable[date] || {title: date, items: []};
+        hashTable[date].items.push(recordList[i]);
+      }
+      console.log(hashTable);
+      return hashTable;
+    }
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '-';
     interval = 'day';
     intervalList = intervalList;
@@ -24,16 +58,37 @@
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep .type-tabs-item {
-    background: white;
-    &.selected {
-      background: #c4c4c4;
-      &::after {
-        display: none;
+  ::v-deep {
+    .type-tabs-item {
+      background: white;
+      &.selected {
+        background: #c4c4c4;
+        &::after {
+          display: none;
+        }
       }
     }
+    .interval-tabs-item {
+      height: 48px;
+    }
   }
-  ::v-deep .interval-tabs-item {
-    height: 48px;
+  %item {
+    padding: 0 16px;
+    min-height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .title {
+    @extend %item;
+  }
+  .record {
+    @extend %item;
+    background: white;
+  }
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
   }
 </style>
